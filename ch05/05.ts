dafaef39a9865ab739aad47d4ea35995cd2554b6
Joon <zzzdnjswns@naver.com>
2022-11-21 22:03:17 +0900

    enum

---
+++ ch05/05.ts
@@ -0,0 +1,5 @@
+let member: [string, number] = ["김머사", 10];
+
+let member2: [string, number] = ["김멋사", 1, 10];
+
+export { member };
---
+++ ch07/07.ts
@@ -0,0 +1,29 @@
+enum Class {
+  Rock,
+  Scissors,
+  Paper,
+}
+
+enum Class {
+  Rock2 = 0,
+  Scissors2 = 100 + 1,
+  Paper2,
+}
+
+enum Class {
+  Rock3 = "Rock",
+  Scissors3 = "SCISSORS",
+  Paper3 = "Paper",
+}
+
+enum Game {
+  Rock = "ROCK",
+  Scissors = 2,
+  Paper,
+}
+
+const enum Game2 {
+  Rock,
+  Scissors,
+  Paper,
+}
